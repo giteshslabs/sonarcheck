e03424c0a498bb8ff70d8ae5028f36b358cece90
giteshslabs <gitesh@sookshum-labs.com>
2024-05-03 15:02:37 +0530

    Optimize location

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,9 @@ export class AppService {
   getHello(): string {
     const region = 'North East and Yorkshire';
     const country = 'Wales';
-    const event = `New registered user${country ? ` from ${region ? `${region}, ` : ''}` : region ? ' from ' : ''}${country || region || ''}`;
+    const location =
+      country && region ? `${region}, ${country}` : region || country || '';
+    const event = location ? `New registered user from ${location}` : 'New registered user';
     console.log(`event`, event);
     return 'Hello World!';
   }
