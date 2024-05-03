c34e29a7807de5a2e065862a369125ce95accd61
giteshslabs <gitesh@sookshum-labs.com>
2024-05-03 15:11:48 +0530

    Test for survey

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,11 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const surveyState = 1;
     const region = 'North East and Yorkshire';
     const country = 'Wales';
     const location =
       country && region ? `${region}, ${country}` : region || country || '';
-    const event = location ? `New registered user from ${location}` : 'New registered user';
+    const event = location
+      ? `New T${surveyState} survey from ${location}`
+      : `New T${surveyState} survey`;
     console.log(`event`, event);
     return 'Hello World!';
   }
