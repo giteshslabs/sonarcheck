5f4437b1d9ffd6119d039043eb0159939546e8c1
giteshslabs <gitesh@sookshum-labs.com>
2024-06-21 09:58:27 +0530

    fix middleware

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -1,18 +1,10 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
-import { URL, parse } from 'url';
 
 @Injectable()
 export class AppMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    // The signature '(urlString: string): UrlWithStringQuery' of 'url.parse' is deprecated.
-    const parsedUrl = parse(req.originalUrl).pathname;
-    console.log(`parsedUrl`, parsedUrl, req.route);
-
-    // console.log(`req.url`, req.baseUrl);
-    // const parsedUrl = new URL(req.url);
-
-    // console.log(`parsedUrl`, parsedUrl);
+    console.log(`req.originalUrl`, req.originalUrl);
     next();
   }
 }
